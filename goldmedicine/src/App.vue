5d83f90886856dba0728662c16106ec8d53bcfe0
Zlnc <478044574@qq.com>
2020-01-02 21:33:42 +0800

    add page

--- goldmedicine/src/App.vue
+++ goldmedicine/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderGold />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderGold from "./components/HeaderGold.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HeaderGold
   }
-}
+};
 </script>
+
+
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
+
+
 </style>
---
+++ goldmedicine/src/components/HeaderGold.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="headerGold">
+    <div class="goldCenter headerFlex">
+      <div class="navLinkLogin">欢迎进入金象网</div>
+      <ul>
+        <li class="nameList" v-for="(item,index) in headerName" :key="index" v-text="item.name"></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderGold",
+  data: () => {
+    return {
+      headerName: [
+        {
+          name: "购物车"
+        },
+        {
+          name: "我的订单"
+        },
+        {
+          name: "个人中心"
+        },
+        {
+          name: "关注金象网"
+        },
+        {
+          name: "客户服务"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.headerGold {
+  height: 30px;
+  background: #f3f3f3;
+  line-height: 30px;
+}
+
+.navLinkLogin {
+  width: 190px;
+  height: 100%;
+}
+
+.headerFlex {
+  display: flex;
+}
+
+ul {
+  flex: 1;
+  margin: 0;
+  padding: 0;
+}
+.nameList {
+  float: left;
+  line-height: 30px;
+}
+</style>
